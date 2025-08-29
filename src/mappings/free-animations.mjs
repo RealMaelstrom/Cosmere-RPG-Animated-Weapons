ed40c5b14addd89027dc76153a5ee90cfbd232af
Khunkurisu <creative.3d@live.com>
2025-08-28 21:53:28 -0500

    chore: format the mapping files more nicely
    Signed-off-by: Khunkurisu <creative.3d@live.com>

--- src/mappings/free-animations.mjs
+++ src/mappings/free-animations.mjs
@@ -1,185 +1,185 @@
 export const mappings = {
-  "axe": {
-    file: "jb2a.melee_attack.02.battleaxe.01.0",
-    scale: 1.0,
-    location: "token",
-    stretchTo: "target",
-    duration: 1533,
-  },
+	"axe": {
+		file: "jb2a.melee_attack.02.battleaxe.01.0",
+		scale: 1.0,
+		location: "token",
+		stretchTo: "target",
+		duration: 1533,
+	},
 
-  "crossbow": {
-    file: "jb2a.bolt.physical.orange",
-    scale: 0.8,
-    location: "token",
-    stretchTo: "target",
-    duration: 1400,
-  },
+	"crossbow": {
+		file: "jb2a.bolt.physical.orange",
+		scale: 0.8,
+		location: "token",
+		stretchTo: "target",
+		duration: 1400,
+	},
 
-   "grandbow": {
-    file: "jb2a.arrow.physical.blue",
-    scale: 1.5,
-    location: "token",
-    stretchTo: "target",
-    duration: 1200,
-  },
+	"grandbow": {
+		file: "jb2a.arrow.physical.blue",
+		scale: 1.5,
+		location: "token",
+		stretchTo: "target",
+		duration: 1200,
+	},
 
-  "greatsword": {
-    file: "jb2a.greatsword.melee.standard.white",
-    scale: 1.2,
-    location: "token",
-    stretchTo: "target",
-    duration: 2400,
-  },
+	"greatsword": {
+		file: "jb2a.greatsword.melee.standard.white",
+		scale: 1.2,
+		location: "token",
+		stretchTo: "target",
+		duration: 2400,
+	},
 
-  "half-shard": {
-    file: "jb2a.melee_attack.06.shield.01.1",
-    scale: 1.5,
-    location: "token",
-    stretchTo: "target",
-    duration: 1100,
-  },
+	"half-shard": {
+		file: "jb2a.melee_attack.06.shield.01.1",
+		scale: 1.5,
+		location: "token",
+		stretchTo: "target",
+		duration: 1100,
+	},
 
-  "hammer": {
-    file: "jb2a.warhammer.melee.01.white.4",
-    scale: 1.5,
-    location: "token",
-    stretchTo: "target",
-    duration: 2500,
-  },
+	"hammer": {
+		file: "jb2a.warhammer.melee.01.white.4",
+		scale: 1.5,
+		location: "token",
+		stretchTo: "target",
+		duration: 2500,
+	},
 
-  "javelin": {
-    file: "jb2a.arrow.physical.white.01",
-    scale: 5,
-    location: "token",
-    stretchTo: "target",
-    duration: 1500,
-  },
+	"javelin": {
+		file: "jb2a.arrow.physical.white.01",
+		scale: 5,
+		location: "token",
+		stretchTo: "target",
+		duration: 1500,
+	},
 
-  "knife": {
-    file: "jb2a.dagger.melee.02.white",
-    scale: 1.0,
-    location: "token",
-    stretchTo: "target",
-    duration: 1500,
-  },
+	"knife": {
+		file: "jb2a.dagger.melee.02.white",
+		scale: 1.0,
+		location: "token",
+		stretchTo: "target",
+		duration: 1500,
+	},
 
-  "longbow": {
-    file: "jb2a.arrow.physical.blue",
-    scale: 1.2,
-    location: "token",
-    stretchTo: "target",
-    duration: 1200,
-  },
+	"longbow": {
+		file: "jb2a.arrow.physical.blue",
+		scale: 1.2,
+		location: "token",
+		stretchTo: "target",
+		duration: 1200,
+	},
 
-  "longspear": {
-    file: "jb2a.spear.melee.01.white.5",
-    scale: 1.3,
-    location: "token",
-    stretchTo: "target",
-    duration: 2500,
-  },
+	"longspear": {
+		file: "jb2a.spear.melee.01.white.5",
+		scale: 1.3,
+		location: "token",
+		stretchTo: "target",
+		duration: 2500,
+	},
 
-  "longsword": {
-    file: "jb2a.sword.melee.01.white.5",
-    scale: 1.2,
-    location: "token",
-    stretchTo: "target",
-    duration: 2500,
-  },
+	"longsword": {
+		file: "jb2a.sword.melee.01.white.5",
+		scale: 1.2,
+		location: "token",
+		stretchTo: "target",
+		duration: 2500,
+	},
 
-  "mace": {
-    file: "jb2a.mace.melee.01.white.0",
-    scale: 1.2,
-    location: "token",
-    stretchTo: "target",
-    duration: 2200,
-  },
+	"mace": {
+		file: "jb2a.mace.melee.01.white.0",
+		scale: 1.2,
+		location: "token",
+		stretchTo: "target",
+		duration: 2200,
+	},
 
-  "poleaxe": {
-    file: "jb2a.halberd.melee.01.white.3",
-    scale: 1.3,
-    location: "token",
-    stretchTo: "target",
-    duration: 2000,
-  },
+	"poleaxe": {
+		file: "jb2a.halberd.melee.01.white.3",
+		scale: 1.3,
+		location: "token",
+		stretchTo: "target",
+		duration: 2000,
+	},
 
-   "rapier": {
-    file: "jb2a.rapier.melee.01.white.3",
-    scale: 1.3,
-    location: "token",
-    stretchTo: "target",
-    duration: 2700,
-  },
+	"rapier": {
+		file: "jb2a.rapier.melee.01.white.3",
+		scale: 1.3,
+		location: "token",
+		stretchTo: "target",
+		duration: 2700,
+	},
 
-  "shardblade": {
-    file: "jb2a.melee_attack.03.magical_greatsword.01.orange.0",
-    scale: 1.8,
-    location: "token",
-    stretchTo: "target",
-    duration: 1700,
-  },
+	"shardblade": {
+		file: "jb2a.melee_attack.03.magical_greatsword.01.orange.0",
+		scale: 1.8,
+		location: "token",
+		stretchTo: "target",
+		duration: 1700,
+	},
 
-    "shield": {
-    file: "jb2a.melee_attack.06.shield.01.0",
-    scale: 1.5,
-    location: "token",
-    stretchTo: "target",
-    duration: 1200,
-  },
+	"shield": {
+		file: "jb2a.melee_attack.06.shield.01.0",
+		scale: 1.5,
+		location: "token",
+		stretchTo: "target",
+		duration: 1200,
+	},
 
-  "shortbow": {
-    file: "jb2a.arrow.physical.white.01",
-    scale: .8,
-    location: "token",
-    stretchTo: "target",
-    duration: 1500,
-  },
+	"shortbow": {
+		file: "jb2a.arrow.physical.white.01",
+		scale: .8,
+		location: "token",
+		stretchTo: "target",
+		duration: 1500,
+	},
 
-   "shortspear": {
-    file: "jb2a.spear.melee.01.white.2",
-    scale: .8,
-    location: "token",
-    stretchTo: "target",
-    duration: 2200,
-  },
+	"shortspear": {
+		file: "jb2a.spear.melee.01.white.2",
+		scale: .8,
+		location: "token",
+		stretchTo: "target",
+		duration: 2200,
+	},
 
-   "sidesword": {
-    file: "jb2a.shortsword.melee.01.white.5",
-    scale: 1.3,
-    location: "token",
-    stretchTo: "target",
-    duration: 2867,
-  },
+	"sidesword": {
+		file: "jb2a.shortsword.melee.01.white.5",
+		scale: 1.3,
+		location: "token",
+		stretchTo: "target",
+		duration: 2867,
+	},
 
-  "sling": {
-    file: "jb2a.bullet.01.orange",
-    scale: 1.0,
-    location: "token",
-    stretchTo: "target",
-    duration: 533,
-  },
+	"sling": {
+		file: "jb2a.bullet.01.orange",
+		scale: 1.0,
+		location: "token",
+		stretchTo: "target",
+		duration: 533,
+	},
 
-  "staff": {
-    file: "jb2a.quarterstaff.melee.01.white.1",
-    scale: 1.3,
-    location: "token",
-    stretchTo: "target",
-    duration: 2700,
-  },
+	"staff": {
+		file: "jb2a.quarterstaff.melee.01.white.1",
+		scale: 1.3,
+		location: "token",
+		stretchTo: "target",
+		duration: 2700,
+	},
 
-  "unarmed strike": {
-    file: "jb2a.unarmed_strike.physical.01.blue",
-    scale: 1.5,
-    location: "token",
-    stretchTo: "target",
-    duration: 1033,
-  },
+	"unarmed strike": {
+		file: "jb2a.unarmed_strike.physical.01.blue",
+		scale: 1.5,
+		location: "token",
+		stretchTo: "target",
+		duration: 1033,
+	},
 
- "warhammer": {
-    file: "jb2a.melee_attack.02.hammer.02.0",
-    scale: 2.0,
-    location: "token",
-    stretchTo: "target",
-    duration: 1533,
-  },
+	"warhammer": {
+		file: "jb2a.melee_attack.02.hammer.02.0",
+		scale: 2.0,
+		location: "token",
+		stretchTo: "target",
+		duration: 1533,
+	},
 };
