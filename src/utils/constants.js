570c6645639789270af888d190892c4b62309da2
Khunkurisu <creative.3d@live.com>
2025-08-28 22:44:16 -0500

    chore: move constants to their own file
    Signed-off-by: Khunkurisu <creative.3d@live.com>

---
+++ src/utils/constants.js
@@ -0,0 +1,26 @@
+/**
+ * String identifier for the module used throughout other scripts.
+ */
+export const MODULE_ID = 'cosmere-rpg-animated-weapons';
+
+/**
+ * Full title string of the module.
+ */
+export const MODULE_NAME = 'Cosmere RPG Animated Weapons';
+
+/**
+ * String identifier for the system used throughout other scripts.
+ */
+export const SYSTEM_ID = 'cosmere-rpg';
+
+export const Assets = Object.freeze({
+	None: Symbol("none"),
+	Free: Symbol("free"),
+	Premium: Symbol("premium"),
+});
+
+
+export const SETTINGS = {
+	AnimationMappings: "animationAssetMappings",
+	MappingSync: "mappingSync",
+}
