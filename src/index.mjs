6f4ce04a7d6c79f42e60bfe7e949b0b0c829b16a
Maelstrom <XDMaelstrom@gmail.com>
2025-08-28 19:25:38 -0700

    feat: generate event macro from mappings

--- src/index.mjs
+++ src/index.mjs
@@ -1,40 +1,67 @@
+import { mappings as PremiumMappings } from "./premium-animations.mjs";
+import { mappings as FreeMappings } from "./free-animations.mjs";
+
 const Assets = Object.freeze({
-  None: Symbol("none"),
-  Free: Symbol("free"),
-  Premium: Symbol("premium"),
+    None: Symbol("none"),
+    Free: Symbol("free"),
+    Premium: Symbol("premium"),
 });
 
 var assetState = Assets.None;
 
-Hooks.once('read', () => {
-  if(game.modules.get('jb2a_patreon')?.active) {
-    assetState = Assets.Premium;
-  } else if(game.modules.get('JB2A_DnD5e')?.active) {
-    assetState = Assets.Free;
-  } else {
-    ui.notifications.error("No animation assets detected. Please install either JB2A Free or their Patreon pack!");
-  }
+Hooks.once('ready', () => {
+    if (game.modules.get('jb2a_patreon')?.active) {
+        assetState = Assets.Premium;
+    } else if (game.modules.get('JB2A_DnD5e')?.active) {
+        assetState = Assets.Free;
+    } else {
+        ui.notifications.error("No animation assets detected. Please install either JB2A Free or their Patreon pack!");
+    }
 });
 
 Hooks.on('createItem', (item, options, userId) => {
-  if(item.parent) {
-    switch(assetState) {
-      case Assets.Premium: {
-        assignPremiumAssets(item);
-        break;
-      }
-      case Assets.Free: {
-        assignFreeAssets(item);
-        break;
-      }
+    if (item.parent) {
+        switch (assetState) {
+            case Assets.Premium: {
+                assignAssets(item, PremiumMappings);
+                break;
+            }
+            case Assets.Free: {
+                assignAssets(item, FreeMappings);
+                break;
+            }
+        }
     }
-  }
 });
 
-function assignPremiumAssets(item) {
-  // stuff here
-}
+function assignAssets(item, mappings) {
+    const mapping = mappings[item.system.id];
+    if (!mapping) {
+        console.warn(`Unable to find mapping for ${item.system.id}.`);
+        return;
+    }
 
-function assignFreeAssets(item) {
-  // stuff here
+    var macro = "let target = Array.from(game.user.targets)[0];\n";
+    macro += "if (!target) {\n\t"
+    macro += "ui.notifications.error('Animation requires a target!');\n\treturn;\n}\n\n";
+    macro += "new Sequence().effect()\n\t";
+    macro += `.file("${mapping.file}")\n\t.scale(${mapping.scale})\n\t.atLocation(${mapping.location})\n\t`;
+    macro += `.stretchTo(${mapping.stretchTo})\n\t.duration(${mapping.duration})\n\t.play();`;
+
+    const events = item.system.events;
+    events[`${item.system.id}-animation-macro`] = {
+        "id": "AaPJGHg3XlkXrPde",
+        "description": `${item.name} Animation`,
+        "event": "use",
+        "handler": {
+            "type": "execute-macro",
+            "inline": true,
+            "macro": {
+                "type": "script",
+                "command": macro,
+            }
+        },
+    };
+
+    item.update({ 'system.events': events });
 }
