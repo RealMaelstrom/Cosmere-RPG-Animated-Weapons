76aec9c4fc7b8c54de18e0b345552e00bc9781a1
Khunkurisu <creative.3d@live.com>
2025-08-29 22:29:33 -0500

    feat: save current module and system version in a setting for reference
    Signed-off-by: Khunkurisu <creative.3d@live.com>

--- src/macros/mapping.js
+++ src/macros/mapping.js
@@ -6,15 +6,21 @@ export function DetectMapping() {
 	} else if (game.modules.get('JB2A_DnD5e')?.active) {
 		return Assets.Free;
 	} else {
-		ui.notifications.error("No animation assets detected. Please install either JB2A Free or their Patreon pack!");
+		ui.notifications.error("cosmere-animations.notifications.noAnimationAssetsError");
 		return Assets.None;
 	}
 }
 
 export function SyncMapping() {
+	// to-do, add custom mappings in settings and detect them
+	// so they aren't overridden by this sync
 	const mapping = GetMapping();
 	if (mapping) {
 		game.settings.set(MODULE_ID, SETTINGS.AnimationMappings, mapping);
+		game.settings.set(MODULE_ID, SETTINGS.MappingSync, {
+			system: game.system.version,
+			module: game.modules.get(MODULE_ID).version,
+		});
 	}
 }
 
