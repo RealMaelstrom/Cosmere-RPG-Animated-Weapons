267567cf2791e4ee3ed61a7f61c393e9fc5833a7
Khunkurisu <creative.3d@live.com>
2025-08-29 22:30:04 -0500

    feat: update mappings to use new structure
    Signed-off-by: Khunkurisu <creative.3d@live.com>

--- src/mappings/free-animations.mjs
+++ src/mappings/free-animations.mjs
@@ -3,7 +3,8 @@ export const mappings = {
 		file: "jb2a.melee_attack.02.battleaxe.01.0",
 		scale: 1.0,
 		location: "token",
-		stretchTo: "target",
+		stretchToTarget: false,
+		rotateToTarget: true,
 		duration: 1533,
 	},
 
@@ -11,7 +12,8 @@ export const mappings = {
 		file: "jb2a.bolt.physical.orange",
 		scale: 0.8,
 		location: "token",
-		stretchTo: "target",
+		stretchToTarget: true,
+		rotateToTarget: true,
 		duration: 1400,
 	},
 
@@ -19,7 +21,8 @@ export const mappings = {
 		file: "jb2a.arrow.physical.blue",
 		scale: 1.5,
 		location: "token",
-		stretchTo: "target",
+		stretchToTarget: true,
+		rotateToTarget: true,
 		duration: 1200,
 	},
 
@@ -27,7 +30,8 @@ export const mappings = {
 		file: "jb2a.greatsword.melee.standard.white",
 		scale: 1.2,
 		location: "token",
-		stretchTo: "target",
+		stretchToTarget: false,
+		rotateToTarget: true,
 		duration: 2400,
 	},
 
@@ -35,7 +39,8 @@ export const mappings = {
 		file: "jb2a.melee_attack.06.shield.01.1",
 		scale: 1.5,
 		location: "token",
-		stretchTo: "target",
+		stretchToTarget: false,
+		rotateToTarget: true,
 		duration: 1100,
 	},
 
@@ -43,7 +48,8 @@ export const mappings = {
 		file: "jb2a.warhammer.melee.01.white.4",
 		scale: 1.5,
 		location: "token",
-		stretchTo: "target",
+		stretchToTarget: false,
+		rotateToTarget: true,
 		duration: 2500,
 	},
 
@@ -51,7 +57,8 @@ export const mappings = {
 		file: "jb2a.arrow.physical.white.01",
 		scale: 5,
 		location: "token",
-		stretchTo: "target",
+		stretchToTarget: true,
+		rotateToTarget: true,
 		duration: 1500,
 	},
 
@@ -59,7 +66,8 @@ export const mappings = {
 		file: "jb2a.dagger.melee.02.white",
 		scale: 1.0,
 		location: "token",
-		stretchTo: "target",
+		stretchToTarget: false,
+		rotateToTarget: true,
 		duration: 1500,
 	},
 
@@ -67,7 +75,8 @@ export const mappings = {
 		file: "jb2a.arrow.physical.blue",
 		scale: 1.2,
 		location: "token",
-		stretchTo: "target",
+		stretchToTarget: true,
+		rotateToTarget: true,
 		duration: 1200,
 	},
 
@@ -75,7 +84,8 @@ export const mappings = {
 		file: "jb2a.spear.melee.01.white.5",
 		scale: 1.3,
 		location: "token",
-		stretchTo: "target",
+		stretchToTarget: false,
+		rotateToTarget: true,
 		duration: 2500,
 	},
 
@@ -83,7 +93,8 @@ export const mappings = {
 		file: "jb2a.sword.melee.01.white.5",
 		scale: 1.2,
 		location: "token",
-		stretchTo: "target",
+		stretchToTarget: false,
+		rotateToTarget: true,
 		duration: 2500,
 	},
 
@@ -91,7 +102,8 @@ export const mappings = {
 		file: "jb2a.mace.melee.01.white.0",
 		scale: 1.2,
 		location: "token",
-		stretchTo: "target",
+		stretchToTarget: false,
+		rotateToTarget: true,
 		duration: 2200,
 	},
 
@@ -99,7 +111,8 @@ export const mappings = {
 		file: "jb2a.halberd.melee.01.white.3",
 		scale: 1.3,
 		location: "token",
-		stretchTo: "target",
+		stretchToTarget: false,
+		rotateToTarget: true,
 		duration: 2000,
 	},
 
@@ -107,7 +120,8 @@ export const mappings = {
 		file: "jb2a.rapier.melee.01.white.3",
 		scale: 1.3,
 		location: "token",
-		stretchTo: "target",
+		stretchToTarget: false,
+		rotateToTarget: true,
 		duration: 2700,
 	},
 
@@ -115,7 +129,8 @@ export const mappings = {
 		file: "jb2a.melee_attack.03.magical_greatsword.01.orange.0",
 		scale: 1.8,
 		location: "token",
-		stretchTo: "target",
+		stretchToTarget: false,
+		rotateToTarget: true,
 		duration: 1700,
 	},
 
@@ -123,7 +138,8 @@ export const mappings = {
 		file: "jb2a.melee_attack.06.shield.01.0",
 		scale: 1.5,
 		location: "token",
-		stretchTo: "target",
+		stretchToTarget: false,
+		rotateToTarget: true,
 		duration: 1200,
 	},
 
@@ -131,7 +147,8 @@ export const mappings = {
 		file: "jb2a.arrow.physical.white.01",
 		scale: .8,
 		location: "token",
-		stretchTo: "target",
+		stretchToTarget: true,
+		rotateToTarget: true,
 		duration: 1500,
 	},
 
@@ -139,7 +156,8 @@ export const mappings = {
 		file: "jb2a.spear.melee.01.white.2",
 		scale: .8,
 		location: "token",
-		stretchTo: "target",
+		stretchToTarget: false,
+		rotateToTarget: true,
 		duration: 2200,
 	},
 
@@ -147,7 +165,8 @@ export const mappings = {
 		file: "jb2a.shortsword.melee.01.white.5",
 		scale: 1.3,
 		location: "token",
-		stretchTo: "target",
+		stretchToTarget: false,
+		rotateToTarget: true,
 		duration: 2867,
 	},
 
@@ -155,7 +174,8 @@ export const mappings = {
 		file: "jb2a.bullet.01.orange",
 		scale: 1.0,
 		location: "token",
-		stretchTo: "target",
+		stretchToTarget: true,
+		rotateToTarget: true,
 		duration: 533,
 	},
 
@@ -163,7 +183,8 @@ export const mappings = {
 		file: "jb2a.quarterstaff.melee.01.white.1",
 		scale: 1.3,
 		location: "token",
-		stretchTo: "target",
+		stretchToTarget: false,
+		rotateToTarget: true,
 		duration: 2700,
 	},
 
@@ -171,7 +192,8 @@ export const mappings = {
 		file: "jb2a.unarmed_strike.physical.01.blue",
 		scale: 1.5,
 		location: "token",
-		stretchTo: "target",
+		stretchToTarget: false,
+		rotateToTarget: true,
 		duration: 1033,
 	},
 
@@ -179,7 +201,8 @@ export const mappings = {
 		file: "jb2a.melee_attack.02.hammer.02.0",
 		scale: 2.0,
 		location: "token",
-		stretchTo: "target",
+		stretchToTarget: false,
+		rotateToTarget: true,
 		duration: 1533,
 	},
 };
--- src/mappings/premium-animations.mjs
+++ src/mappings/premium-animations.mjs
@@ -3,7 +3,8 @@ export const mappings = {
 		file: "jb2a.melee_attack.02.battleaxe.01.0",
 		scale: 1.0,
 		location: "token",
-		stretchTo: "target",
+		stretchToTarget: false,
+		rotateToTarget: true,
 		duration: 1533,
 	},
 
@@ -11,7 +12,8 @@ export const mappings = {
 		file: "jb2a.bolt.physical.white",
 		scale: 0.8,
 		location: "token",
-		stretchTo: "target",
+		stretchToTarget: true,
+		rotateToTarget: true,
 		duration: 1500,
 	},
 
@@ -19,7 +21,8 @@ export const mappings = {
 		file: "jb2a.arrow.physical.white.02",
 		scale: 1.5,
 		location: "token",
-		stretchTo: "target",
+		stretchToTarget: true,
+		rotateToTarget: true,
 		duration: 1500,
 	},
 
@@ -27,7 +30,8 @@ export const mappings = {
 		file: "jb2a.greatsword.melee.standard.white",
 		scale: 1.2,
 		location: "token",
-		stretchTo: "target",
+		stretchToTarget: false,
+		rotateToTarget: true,
 		duration: 2400,
 	},
 
@@ -35,7 +39,8 @@ export const mappings = {
 		file: "jb2a.melee_attack.06.shield.02.0",
 		scale: 1.5,
 		location: "token",
-		stretchTo: "target",
+		stretchToTarget: false,
+		rotateToTarget: true,
 		duration: 3200,
 	},
 
@@ -43,7 +48,8 @@ export const mappings = {
 		file: "jb2a.warhammer.melee.01.white.4",
 		scale: 1.5,
 		location: "token",
-		stretchTo: "target",
+		stretchToTarget: false,
+		rotateToTarget: true,
 		duration: 2500,
 	},
 
@@ -51,7 +57,8 @@ export const mappings = {
 		file: "jb2a.javelin.throw",
 		scale: 1.2,
 		location: "token",
-		stretchTo: "target",
+		stretchToTarget: true,
+		rotateToTarget: true,
 		duration: 1500,
 	},
 
@@ -59,7 +66,8 @@ export const mappings = {
 		file: "jb2a.dagger.melee.02.white",
 		scale: 1.0,
 		location: "token",
-		stretchTo: "target",
+		stretchToTarget: false,
+		rotateToTarget: true,
 		duration: 1500,
 	},
 
@@ -67,7 +75,8 @@ export const mappings = {
 		file: "jb2a.arrow.physical.white.02",
 		scale: 1.0,
 		location: "token",
-		stretchTo: "target",
+		stretchToTarget: true,
+		rotateToTarget: true,
 		duration: 1200,
 	},
 
@@ -75,7 +84,8 @@ export const mappings = {
 		file: "jb2a.spear.melee.01.white.5",
 		scale: 1.3,
 		location: "token",
-		stretchTo: "target",
+		stretchToTarget: false,
+		rotateToTarget: true,
 		duration: 2500,
 	},
 
@@ -83,7 +93,8 @@ export const mappings = {
 		file: "jb2a.sword.melee.01.white.5",
 		scale: 1.2,
 		location: "token",
-		stretchTo: "target",
+		stretchToTarget: false,
+		rotateToTarget: true,
 		duration: 2500,
 	},
 
@@ -91,7 +102,8 @@ export const mappings = {
 		file: "jb2a.mace.melee.01.white.0",
 		scale: 1.2,
 		location: "token",
-		stretchTo: "target",
+		stretchToTarget: false,
+		rotateToTarget: true,
 		duration: 2200,
 	},
 
@@ -99,7 +111,8 @@ export const mappings = {
 		file: "jb2a.halberd.melee.01.white.3",
 		scale: 1.3,
 		location: "token",
-		stretchTo: "target",
+		stretchToTarget: false,
+		rotateToTarget: true,
 		duration: 2000,
 	},
 
@@ -107,7 +120,8 @@ export const mappings = {
 		file: "jb2a.rapier.melee.01.white.3",
 		scale: 1.3,
 		location: "token",
-		stretchTo: "target",
+		stretchToTarget: false,
+		rotateToTarget: true,
 		duration: 2700,
 	},
 
@@ -115,7 +129,8 @@ export const mappings = {
 		file: "jb2a.sword.melee.fire.blue",
 		scale: 1.8,
 		location: "token",
-		stretchTo: "target",
+		stretchToTarget: false,
+		rotateToTarget: true,
 		duration: 2033,
 	},
 
@@ -123,7 +138,8 @@ export const mappings = {
 		file: "jb2a.melee_attack.06.shield.01.0",
 		scale: 1.5,
 		location: "token",
-		stretchTo: "target",
+		stretchToTarget: false,
+		rotateToTarget: true,
 		duration: 1667,
 	},
 
@@ -131,7 +147,8 @@ export const mappings = {
 		file: "jb2a.arrow.physical.white.01",
 		scale: .8,
 		location: "token",
-		stretchTo: "target",
+		stretchToTarget: true,
+		rotateToTarget: true,
 		duration: 1500,
 	},
 
@@ -139,7 +156,8 @@ export const mappings = {
 		file: "jb2a.spear.melee.01.white.2",
 		scale: .8,
 		location: "token",
-		stretchTo: "target",
+		stretchToTarget: false,
+		rotateToTarget: true,
 		duration: 2200,
 	},
 
@@ -147,7 +165,8 @@ export const mappings = {
 		file: "jb2a.shortsword.melee.01.white.5",
 		scale: 1.3,
 		location: "token",
-		stretchTo: "target",
+		stretchToTarget: false,
+		rotateToTarget: true,
 		duration: 2867,
 	},
 
@@ -155,7 +174,8 @@ export const mappings = {
 		file: "jb2a.slingshot",
 		scale: 1.0,
 		location: "token",
-		stretchTo: "target",
+		stretchToTarget: true,
+		rotateToTarget: true,
 		duration: 3033,
 	},
 
@@ -163,7 +183,8 @@ export const mappings = {
 		file: "jb2a.quarterstaff.melee.02.white.1",
 		scale: 1.3,
 		location: "token",
-		stretchTo: "target",
+		stretchToTarget: false,
+		rotateToTarget: true,
 		duration: 2700,
 	},
 
@@ -171,7 +192,8 @@ export const mappings = {
 		file: "jb2a.unarmed_strike.physical.02.orange",
 		scale: 1.5,
 		location: "token",
-		stretchTo: "target",
+		stretchToTarget: false,
+		rotateToTarget: true,
 		duration: 1033,
 	},
 
@@ -179,7 +201,8 @@ export const mappings = {
 		file: "jb2a.melee_attack.02.hammer.02.0",
 		scale: 2.0,
 		location: "token",
-		stretchTo: "target",
+		stretchToTarget: false,
+		rotateToTarget: true,
 		duration: 1533,
 	},
 };
